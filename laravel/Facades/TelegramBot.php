6805bfd9f70d35e15c40b6a519f36d535764b4ba
moorlex <moorlex@mail.com>
2024-10-01 17:42:36 +0300

    v3

--- laravel/Facades/TelegramBot.php
+++ laravel/Facades/TelegramBot.php
@@ -3,6 +3,9 @@
 namespace LaMoore\Tg\Laravel\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use LaMoore\Tg\BotModules\BotCommands;
+use LaMoore\Tg\BotModules\BotEvents;
+use LaMoore\Tg\BotModules\BotLogger;
 use LaMoore\Tg\Enums\UpdateTypes;
 use LaMoore\Tg\TelegramApi;
 use LaMoore\Tg\TelegramUpdate;
@@ -11,13 +14,14 @@ use LaMoore\Tg\Chat\TelegramChat;
 /**
  * @param string $token
  * @param int $id
+ * @param BotCommands $commands
+ * @param BotEvents $events
+ * @param BotLogger $logger
  * @param TelegramApi $api
  * @param TelegramUpdate $update
  * @param TelegramChat $chat
- * @method static void command(string $command, array|callable $callback)
+ * @method static void handleCommand(string $command, array|callable $callback)
  * @method static void on(UpdateTypes $event, array|callable $callback)
- * @method static void clearListeners()
- * @method static void clearCommands()
  * @method static mixed handle(array $request)
  */
 class TelegramBot extends Facade
